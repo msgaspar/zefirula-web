76f612b6c64534eb408993c7484e9e0539872d27
Matheus Gaspar <msouzagaspar@gmail.com>
2021-05-21 12:04:34 -0300

    add form validation on create league page

--- src/pages/leagues/create.tsx
+++ src/pages/leagues/create.tsx
@@ -1,10 +1,32 @@
-import Header from '../../components/Header'
-import { Input } from '../../components/Form/Input'
 import { Box, Flex, Heading, Divider, SimpleGrid, VStack, HStack, Button } from '@chakra-ui/react'
-import { Sidebar } from '../../components/Sidebar'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
 import Link from 'next/link'
 
+import { Input } from '../../components/Form/Input'
+import Header from '../../components/Header'
+import { Sidebar } from '../../components/Sidebar'
+
+type CreateLeagueFormData = {
+  name: string;
+}
+
+const createLeagueFormSchema = yup.object().shape({
+  name: yup.string().required('Digite um nome para a nova liga')
+})
+
 export default function CreateLeague() {
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(createLeagueFormSchema)
+  })
+
+  const { errors } = formState;
+
+  const handleCreateLeague: SubmitHandler<CreateLeagueFormData> = async (values) => {
+    await new Promise(resolve => setTimeout(resolve, 2000))
+  }
+
   return (
     <Box>
       <Header />
@@ -12,14 +34,25 @@ export default function CreateLeague() {
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
 
-        <Box flex="1" borderRadius={8} bg="gray.800" p={["6", "8"]}>
+        <Box
+          as="form"
+          flex="1"
+          borderRadius={8}
+          bg="gray.800"
+          p={["6", "8"]}
+          onSubmit={handleSubmit(handleCreateLeague)}
+          >
           <Heading size="lg" fontWeight="normal">Criar nova liga</Heading>
 
           <Divider my="6" borderColor="gray.700" />
 
           <VStack spacing="8">
             <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
-              <Input name="name" label="Nome da liga" />
+              <Input
+                name="name"
+                label="Nome da liga"
+                error={errors.name}
+                {...register('name')} />
             </SimpleGrid>
           </VStack>
 
@@ -28,7 +61,11 @@ export default function CreateLeague() {
               <Link href="/leagues" passHref>
                 <Button colorScheme="whiteAlpha">Cancelar</Button>
               </Link>
-              <Button colorScheme="teal">Salvar</Button>
+              <Button
+                colorScheme="teal"
+                type="submit"
+                isLoading={formState.isSubmitting}
+                >Salvar</Button>
             </HStack>
           </Flex>
         </Box>
