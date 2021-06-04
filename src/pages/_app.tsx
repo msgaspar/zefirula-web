1762c005da7e74046aff818c97b643da64044d0e
Matheus Gaspar <msouzagaspar@gmail.com>
2021-06-04 12:12:54 -0300

    try without the authProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,12 +10,12 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     // <QueryClientProvider client={queryClient}>
     <ChakraProvider theme={theme}>
-      <AuthProvider>
-        {/* <SidebarDrawerProvider> */}
-        <div>Oi</div>
-        {/* <Component {...pageProps} /> */}
-        {/* </SidebarDrawerProvider> */}
-      </AuthProvider>
+      {/* <AuthProvider> */}
+      {/* <SidebarDrawerProvider> */}
+      <div>Oi</div>
+      {/* <Component {...pageProps} /> */}
+      {/* </SidebarDrawerProvider> */}
+      {/* </AuthProvider> */}
     </ChakraProvider>
     // </QueryClientProvider>
   );
